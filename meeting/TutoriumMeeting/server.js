c79fc3f7c2a98f11e10aa63aaae5b8ce833ad2d1
hozgurde <hozgurde@gmail.com>
2022-12-15 02:06:45 +0300

    feat(meeting): add whiteboard and its initial tools

--- meeting/TutoriumMeeting/server.js
+++ meeting/TutoriumMeeting/server.js
@@ -13,26 +13,25 @@ const PORT = 3000;
 
 // make all the files in 'public' available
 // https://expressjs.com/en/starter/static-files.html
-app.use('/scripts', express.static(__dirname + '/node_modules/peerjs/dist/'));
-app.use(express.static(__dirname + '/src'));
-app.use('/public', express.static(__dirname + '/public'));
+app.use("/scripts", express.static(__dirname + "/node_modules"));
+app.use(express.static(__dirname + "/src"));
+app.use("/public", express.static(__dirname + "/public"));
 //app.use(express.static("src"));
-
 
 // https://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/public/index.html");
+    response.sendFile(__dirname + "/public/index.html");
 });
 
 // listen for requests :)
 const listener = app.listen(PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
+    console.log("Your app is listening on port " + listener.address().port);
 });
 
 // peerjs server
 const peerServer = ExpressPeerServer(listener, {
-  debug: true,
-  path: '/myapp'
+    debug: true,
+    path: "/myapp",
 });
 
-app.use('/peerjs', peerServer);
+app.use("/peerjs", peerServer);
--- meeting/TutoriumMeeting/src/meeting.js
+++ meeting/TutoriumMeeting/src/meeting.js
@@ -1,96 +1,320 @@
+//Whiteboard Initialization
+const app = new PIXI.Application({
+    antialias: true,
+    background: "#1099bb",
+    width: 400,
+    height: 300,
+});
 
-//import { Peer } from "peerjs";
-//const { Peer } = require("peerjs");
-//const peer = new Peer("asd");
+window.addEventListener("resize", () => {
+    app.resize(window.width * 2, window.height * 2);
+});
+
+const stage = new PIXI.Container();
+
+app.stage.addChild(stage);
+
+const container = document.querySelector(".white-board");
+
+container.appendChild(app.view);
+
+/*const whiteboard = new PIXI.Graphics();
+whiteboard.beginFill(0xffffff);
+whiteboard.drawRect(0, 0, 800, 600);
+whiteboard.endFill();
+stage.addChild(whiteboard);*/
+
+var sprite = new PIXI.Graphics();
+
+let initPointer = null;
+
+let isMouseButtonDown = false;
+
+const putDistance = 1;
+var curDistance = 1;
+
+var mousePosRef;
+
+/* End of whiteboard Initialization */
 
 let Peer = window.Peer;
 
-
-
-let messagesEl = document.querySelector('.messages');
-let peerIdEl = document.querySelector('#connect-to-peer');
-let videoEl = document.querySelector('.remote-video');
+let messagesEl = document.querySelector(".messages");
+let peerIdEl = document.querySelector("#connect-to-peer");
+let videoEl = document.querySelector(".remote-video");
 let currentCall = null;
 
 let logMessage = (message) => {
-  let newMessage = document.createElement('div');
-  newMessage.innerText = message;
-  messagesEl.appendChild(newMessage);
+    let newMessage = document.createElement("div");
+    newMessage.innerText = message;
+    messagesEl.appendChild(newMessage);
 };
 
 let renderVideo = (stream) => {
-  videoEl.srcObject = stream;
+    videoEl.srcObject = stream;
 };
 
 // Register with the peer server
 let peer = new Peer({
-  host: '/',
-  port: 3000,
-  path: '/peerjs/myapp'
+    host: "/",
+    port: 3000,
+    path: "/peerjs/myapp",
 });
 
-peer.on('open', (id) => {
-  logMessage('My peer ID is: ' + id);
+let otherPeer = null;
+
+let connectionInitiated = false;
+
+peer.on("open", (id) => {
+    logMessage("My peer ID is: " + id);
 });
-peer.on('error', (error) => {
-  console.error(error);
+peer.on("error", (error) => {
+    console.error(error);
 });
 
 // Handle incoming data connection
-peer.on('connection', (conn) => {
-  logMessage('incoming peer connection!');
-  conn.on('data', (data) => {
-    logMessage(`received: ${data}`);
-  });
-  conn.on('open', () => {
-    conn.send('hello!');
-  });
+peer.on("connection", (conn) => {
+    otherPeer = conn;
+    conn.on("data", (data) => {
+        if (!connectionInitiated) {
+            connectionInitiated = true;
+        } else {
+            let splittedMessage = data.split("|");
+            let initX = parseFloat(splittedMessage[0]);
+            let initY = parseFloat(splittedMessage[1]);
+            let finalX = parseFloat(splittedMessage[2]);
+            let finalY = parseFloat(splittedMessage[3]);
+            currentZIndex = parseFloat(splittedMessage[5]);
+
+            sprite = new PIXI.Graphics();
+            if (parseFloat(splittedMessage[4]) == 0) {
+                sprite.lineStyle(2, 0xff0000, 1);
+            } else if (parseFloat(splittedMessage[4]) == 1) {
+                sprite.lineStyle(10, 0x1099bb, 1);
+            }
+            sprite.zIndex = currentZIndex;
+            sprite.moveTo(initX, initY);
+            sprite.lineTo(finalX, finalY);
+
+            stage.addChild(sprite);
+        }
+    });
+    conn.on("open", () => {
+        conn.send("hello!");
+    });
 });
 
 // Handle incoming voice/video connection
-peer.on('call', (call) => {
-  currentCall = call;
-  navigator.mediaDevices.getUserMedia({video: true, audio: true})
-    .then((stream) => {
-      call.answer(stream); // Answer the call with an A/V stream.
-      call.on('stream', renderVideo);
-    })
-    .catch((err) => {
-      console.error('Failed to get local stream', err);
-    });
+peer.on("call", (call) => {
+    currentCall = call;
+    navigator.mediaDevices
+        .getUserMedia({ video: true, audio: true })
+        .then((stream) => {
+            call.answer(stream); // Answer the call with an A/V stream.
+            call.on("stream", renderVideo);
+        })
+        .catch((err) => {
+            console.error("Failed to get local stream", err);
+        });
 });
-
 
 // Initiate outgoing connection
 let connectToPeer = () => {
-  let peerId = peerIdEl.value;
-  logMessage(`Connecting to ${peerId}...`);
-  
-  let conn = peer.connect(peerId);
-  conn.on('data', (data) => {
-    logMessage(`received: ${data}`);
-  });
-  conn.on('open', () => {
-    conn.send('hi!');
-  });
-  
-  navigator.mediaDevices.getUserMedia({video: true, audio: true})
-    .then((stream) => {
-      let call = peer.call(peerId, stream);
-      currentCall = call;
-      call.on('stream', renderVideo);
-    })
-    .catch((err) => {
-      logMessage('Failed to get local stream', err);
+    let peerId = peerIdEl.value;
+
+    let conn = peer.connect(peerId);
+    otherPeer = conn;
+    conn.on("data", (data) => {
+        if (!connectionInitiated) {
+            connectionInitiated = true;
+        } else {
+            let splittedMessage = data.split("|");
+            let initX = parseFloat(splittedMessage[0]);
+            let initY = parseFloat(splittedMessage[1]);
+            let finalX = parseFloat(splittedMessage[2]);
+            let finalY = parseFloat(splittedMessage[3]);
+            currentZIndex = parseFloat(splittedMessage[5]);
+
+            sprite = new PIXI.Graphics();
+            if (parseFloat(splittedMessage[4]) == 0) {
+                sprite.lineStyle(2, 0xff0000, 1);
+            } else if (parseFloat(splittedMessage[4]) == 1) {
+                sprite.lineStyle(10, 0x1099bb, 1);
+            }
+            sprite.zIndex = currentZIndex;
+            sprite.moveTo(initX, initY);
+            sprite.lineTo(finalX, finalY);
+
+            stage.addChild(sprite);
+        }
     });
+
+    navigator.mediaDevices
+        .getUserMedia({
+            video: {
+                mediaSource: "screen",
+            },
+        })
+        .then((stream) => {
+            // Use the stream in your PeerJS connection
+            conn.addStream(stream);
+        });
+
+    conn.on("open", () => {
+        conn.send("hi!");
+    });
+
+    navigator.mediaDevices
+        .getUserMedia({ video: true, audio: true })
+        .then((stream) => {
+            let call = peer.call(peerId, stream);
+            currentCall = call;
+            call.on("stream", renderVideo);
+        })
+        .catch((err) => {
+            logMessage("Failed to get local stream", err);
+        });
 };
 
 // Close the connection for both peers
 let disconnectFromPeer = () => {
-  logMessage('Disconnecting from peer');
-  currentCall.close();
-  peer.disconnect();
+    logMessage("Disconnecting from peer");
+    currentCall.close();
+    peer.disconnect();
 };
 
 window.connectToPeer = connectToPeer;
 window.disconnectFromPeer = disconnectFromPeer;
+
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+navigator.getUserMedia(
+    { video: true, audio: false },
+    function (stream) {
+        // Set the source of the video element to the stream
+        document.getElementById("local-video").srcObject = stream;
+    },
+    function (err) {
+        console.error(err);
+    }
+);
+
+// Whiteboard Part
+
+let currentZIndex = 0;
+let currentPenType = 0;
+
+const changePenType = (type) => {
+    currentPenType = type;
+    currentZIndex++;
+    console.log(currentPenType);
+    console.log(currentZIndex);
+};
+
+window.changePenType = changePenType;
+
+const getMousePos = (event) => {
+    const pos = { x: 0, y: 0 };
+    if (container) {
+        // Get the position and size of the component on the page.
+        const holderOffset = app.view.getBoundingClientRect();
+        pos.x = event.pageX - holderOffset.x;
+        pos.y = event.pageY - holderOffset.y;
+    }
+    return pos;
+};
+
+const onMouseMove = (e) => {
+    if (!isMouseButtonDown) {
+        return;
+    }
+
+    // clearSpriteRef(annoRef)
+    if (initPointer == null) return;
+
+    const curMousePosRef = getMousePos(e);
+    curDistance -= Math.abs(curMousePosRef.x - mousePosRef.x) + Math.abs(curMousePosRef.y - mousePosRef.y);
+
+    if (curDistance > 0) {
+        sprite.clear();
+        if (currentPenType === 0) {
+            sprite.lineStyle(2, 0xff0000, 1);
+        } else if (currentPenType === 1) {
+            sprite.lineStyle(10, 0x1099bb, 1);
+        }
+        sprite.zIndex = currentZIndex;
+        sprite.moveTo(initPointer.x, initPointer.y);
+
+        mousePosRef = curMousePosRef;
+        sprite.lineTo(mousePosRef.x, mousePosRef.y);
+    } else {
+        curDistance = putDistance;
+        sprite = new PIXI.Graphics();
+        if (currentPenType === 0) {
+            sprite.lineStyle(2, 0xff0000, 1);
+        } else if (currentPenType === 1) {
+            sprite.lineStyle(10, 0x1099bb, 1);
+        }
+        sprite.moveTo(initPointer.x, initPointer.y);
+        sprite.zIndex = currentZIndex;
+        mousePosRef = curMousePosRef;
+        otherPeer.send(
+            initPointer.x +
+                "|" +
+                initPointer.y +
+                "|" +
+                mousePosRef.x +
+                "|" +
+                mousePosRef.y +
+                "|" +
+                currentPenType +
+                "|" +
+                currentZIndex
+        );
+        initPointer = curMousePosRef;
+        sprite.lineTo(mousePosRef.x, mousePosRef.y);
+        stage.addChild(sprite);
+    }
+};
+
+const onMouseDown = (e) => {
+    mousePosRef = getMousePos(e);
+    initPointer = mousePosRef;
+
+    sprite = new PIXI.Graphics();
+    if (currentPenType === 0) {
+        sprite.lineStyle(2, 0xff0000, 1);
+    } else if (currentPenType === 1) {
+        sprite.lineStyle(10, 0x1099bb, 1);
+    }
+    sprite.moveTo(initPointer.x, initPointer.y);
+    sprite.lineTo(mousePosRef.x, mousePosRef.y);
+
+    stage.addChild(sprite);
+
+    isMouseButtonDown = true;
+
+    console.log(mousePosRef);
+};
+const onMouseUp = (e) => {
+    otherPeer.send(
+        initPointer.x +
+            "|" +
+            initPointer.y +
+            "|" +
+            getMousePos(e).x +
+            "|" +
+            getMousePos(e).y +
+            "|" +
+            currentPenType +
+            "|" +
+            currentZIndex
+    );
+    isMouseButtonDown = false;
+};
+
+container.addEventListener("mousemove", onMouseMove, 0);
+
+container.addEventListener("mousedown", onMouseDown, 0);
+
+container.addEventListener("mouseup", onMouseUp, 0);
